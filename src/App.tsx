9574fdface1231fcbba85f43522b272c029e7b91
matt-marano <matt.j.marano@gmail.com>
2020-08-04 11:42:03 -0700

    Create index.html initial commit
        - Setting up initial file to test on GitHub pages
        - The goal is to create a simple random faction picker for twilight imperium 4th edition

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>Faction Picker</h1>
+          <h3>
+            Work in Progress. Once completed, will provide an interface to pick
+            factions for the board game Twilight Imperium 4th Edition.
+          </h3>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
