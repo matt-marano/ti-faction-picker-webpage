21d2b2828c69c232b381245936de7871ebf288cb
matt.marano <matt.marano@taxact.com>
2020-08-05 12:04:10 -0700

    Updates

---
+++ precache-manifest.f04d98bf3ba5aa65dce31aa0aea859df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27e5e2bed9fe06d330bde5a8a05277c",
+    "url": "/ti-faction-picker-webpage/index.html"
+  },
+  {
+    "revision": "a9098ba1928a868f0302",
+    "url": "/ti-faction-picker-webpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f097a9998e32503e411",
+    "url": "/ti-faction-picker-webpage/static/js/2.90997892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ti-faction-picker-webpage/static/js/2.90997892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9098ba1928a868f0302",
+    "url": "/ti-faction-picker-webpage/static/js/main.96d55832.chunk.js"
+  },
+  {
+    "revision": "50f0bff5390392945908",
+    "url": "/ti-faction-picker-webpage/static/js/runtime-main.176485ea.js"
+  }
+]);
