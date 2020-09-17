8945ca0aede6237169d24a6c0ca03596aea7c8bf
matt-marano <matt.marano@parivedasolutions.com>
2020-09-17 15:20:31 -0700

    Added factions and rough function component to pick randomly

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+
+function pickFactionRandomly(): string {
+  const factions = [
+    "Arborec",
+    "Barony of Letnev",
+    "Clan of Saar",
+    "Embers of Muaat",
+    "Emirates of Hacan",
+    "Federation of Sol",
+    "Ghosts of Creuss",
+    "L1Z1X Mindnet",
+    "Mentak Coalition",
+    "Naalu Collective",
+    "Nekro Virus",
+    "Sardakk N'orr",
+    "Universities of Jol-Nar",
+    "Winnu",
+    "Xxcha Kingdom",
+    "Yin Brotherhood",
+    "Yssaril Tribes",
+  ];
+
+  return factions[Math.floor(Math.random() * factions.length)];
+}
+
+function Picker() {
+  const [factionPicked, setFactionPicked] = useState("");
+
+  return (
+    <div>
+      <p>Click to pick a faction!</p>
+      <button onClick={() => setFactionPicked(pickFactionRandomly())}>
+        PICK A FACTION
+      </button>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -11,6 +48,7 @@ function App() {
             Work in Progress. Once completed, will provide an interface to pick
             factions for the board game Twilight Imperium 4th Edition.
           </h3>
+          <Picker></Picker>
         </div>
       </header>
     </div>
